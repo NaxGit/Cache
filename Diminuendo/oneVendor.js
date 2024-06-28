05333a0f0ebc6d6bec3eb7f617728b46462075c5
NaxGit <145691347+NaxGit@users.noreply.github.com>
2024-06-28 15:28:03 +0300

    Add files via upload

---
+++ Diminuendo/oneVendor.js
@@ -0,0 +1,79 @@
+const jsVendor = document.getElementById('oneVendor');
+
+let entirety;
+let emphasis;
+
+jsVendor.addEventListener
+(
+	'Sprite', () =>
+{
+	entirety.forEach
+	(
+	(Q,i) =>
+	{
+	let card = document.createElement('div'); card.id = i;
+	let icon = document.createElement('img'); icon.src = Q.iIcon;
+	let name = document.createElement('div'); name.textContent = Q.iName;
+
+	card.className = 'JSfind';
+	card.append(icon, name);
+	fragment.append(card);
+	}
+	)
+
+jsVendor.append(fragment);
+
+}
+);
+
+
+
+jsVendor.addEventListener
+(
+	'click', ({ target }) =>
+{
+	if (target.classList.contains('JSfind'))
+	{
+		if (emphasis) { emphasis.classList.remove('emphasize'); };
+		target.classList.add('emphasize');
+		emphasis = target;
+		CASIO.reset();
+		target.append(CASIO)
+	}
+}
+);
+
+
+
+CASIO.addEventListener
+(
+	'submit', (e) =>
+{
+	e.preventDefault();
+
+
+	
+	let ping = e.target.parentNode.id;
+
+let specific = entirety[ping];
+	specific.Cost = Number(costInput.value);
+	specific.Sell = Number(sellInput.value);
+	specific.Qtys = Number(qtysInput.value);
+
+// worker.postMessage(specific);
+
+worker.postMessage({ tab: Stock, obj: specific });
+
+}
+);
+
+
+
+document.getElementById('Shelf').addEventListener
+(
+	'click', async ({ target }) =>
+{
+const response = await fetch('./library/' + target.textContent + '.JSON');
+if (response) { entirety = await response.json(); jsVendor.dispatchEvent(new Event('Sprite')); }
+
+});
