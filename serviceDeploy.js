5efab7d9fe1737a8cd609a8225426717d54ccff5
NaxGit <145691347+NaxGit@users.noreply.github.com>
2024-06-28 14:00:47 +0300

    Delete serviceDeploy.js

--- serviceDeploy.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator)
-{
-    navigator.serviceWorker.register('./serviceWorker.js', { scope: '/Cache/' })
-    .then(registration =>
-    {console.log('Service Worker registered');})
-    .catch(error =>
-    {console.error('Service Worker registration failed:', error);});
-}
